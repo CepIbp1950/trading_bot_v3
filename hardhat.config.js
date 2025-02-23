69ce7008831447cc30a09ef1646673e01f7ae6cd
Clifford Pope <94631438+CepIbp1950@users.noreply.github.com>
2025-02-23 17:06:32 -0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
 
+console.log("ALCHEMY_API_KEY:", process.env.ALCHEMY_API_KEY);
+console.log("PRIVATE_KEY:", process.env.PRIVATE_KEY);
+
 const privateKey = process.env.PRIVATE_KEY || "";
 
 /** @type import('hardhat/config').HardhatUserConfig */
