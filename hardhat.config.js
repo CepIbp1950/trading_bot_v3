ed21dd07565c40b3bee015af7940406de834d03f
clifford pope <cpope@ijjc.com>
2025-02-25 14:09:22 -0500

    Save local changes before merging

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,11 @@
-require("dotenv").config()
-require("@nomicfoundation/hardhat-toolbox")
+// filepath: /Users/cpope/trading_bot_v3/hardhat.config.js
+require("dotenv").config();
+require("@nomicfoundation/hardhat-toolbox");
 
-const privateKey = process.env.PRIVATE_KEY || ""
+console.log("ALCHEMY_API_KEY:", process.env.ALCHEMY_API_KEY);
+console.log("PRIVATE_KEY:", process.env.PRIVATE_KEY);
+
+const privateKey = process.env.PRIVATE_KEY || "";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -9,8 +13,8 @@ module.exports = {
   networks: {
     hardhat: {
       forking: {
-        url: `https://arb-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-        blockNumber: 223528000
+        url: `https://arb-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+        blockNumber: 126673326// Update this to a valid block number
       },
     }
   }
